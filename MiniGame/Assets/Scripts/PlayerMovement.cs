63450211e85c5d3aa7af456ba50b0ee9e971cafe
yisongzhangqwq <yisongzhangqwq@users.noreply.github.com>
2020-10-25 21:27:39 +0800

    1025

---
+++ MiniGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,9 @@
+﻿public class PlayerMovement：MonoBehaviour {
+#region PUBLIC_VAR
+#endregion
+#region PRIVATE_VAR
+[SerializeField] private Transform attackPoint;
+[SerializeField] private float attackRadius;
+[SerializeField] private float movementSpeed;
+[SerializeField] private int damage;
+#endregion
