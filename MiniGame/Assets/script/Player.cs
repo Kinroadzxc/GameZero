961fb89a1b70ee2377ff9a5d043a0d9fc6d90164
yisongzhangqwq <yisongzhangqwq@users.noreply.github.com>
2020-10-26 22:32:33 +0800

    添加左右行走

---
+++ MiniGame/Assets/script/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5;
+    private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+
+    private float x;
+    private float y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        Run();
+    }
+    private void Run()
+    {
+        Vector3 movement = new Vector3(x,y,z:0);
+        _rigidbody2D.transform.position += movement * speed * Time.deltaTime;
+    }
+}
