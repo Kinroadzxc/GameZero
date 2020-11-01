9a0185840b1eb54d80dc0dd1426d1fd84c2a3a34
yisongzhangqwq <yisongzhangqwq@users.noreply.github.com>
2020-11-01 20:51:08 +0800

    2d

---
+++ MiniGame/Assets/script/playernew.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playernew : MonoBehaviour
+{
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX =Input.GetAxisRaw("Horizontal");//控制水平方向 A:-1 D:1 0
+        float moveY =Input.GetAxisRaw("Vertical");//控制垂直方向 W:1 S:-1 0
+
+        Vector2 position = transform.position;
+        position.x += moveX * speed * Time.deltaTime;
+        position.y += moveX * speed * Time.deltaTime;
+        transform.position = position;
+
+    }
+}
